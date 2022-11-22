46c05b54953b0e03e4c4da26285d5716b8667bcc
robbfreitas <robertocompromissos@gmail.com>
2022-11-22 15:50:51 -0300

    Adicionar arquivos de projeto.

---
+++ 01TiposDeDados/Program.cs
@@ -0,0 +1,27 @@
+﻿//Tipos de Dados básicos
+
+bool completo = false;
+char genero = 'F';
+char letra = '\u0041'; // \u para representar que vem caractere unicode.
+byte n1 = 126;
+int n2 = 1000;
+int n3 = 2147483647;
+long n4 = 2147483648L; // Convenção usar L ao final.
+float n5 = 4.5f;
+double n6 = 4.5;
+String nome = "Maria Green";
+Object obj1 = "Alex Brown"; // Aceita qualquer valor.
+Object obj2 = 4.5f;
+
+Console.WriteLine(completo);
+Console.WriteLine(genero);
+Console.WriteLine(letra);
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+Console.WriteLine(n4);
+Console.WriteLine(n5);
+Console.WriteLine(n6);
+Console.WriteLine(nome);
+Console.WriteLine(obj1);
+Console.WriteLine(obj2);
