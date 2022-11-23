4c5fa60f67dee07d188d64a96976579f22bff2aa
robbfreitas <robertocompromissos@gmail.com>
2022-11-23 10:27:14 -0300

    Conversão implícita, explícita e Casting.

---
+++ 05ConversaoImplicitaECasting/Program.cs
@@ -0,0 +1,31 @@
+﻿class Casting
+{
+    static void Main(string[] args)
+    {
+        int a = 8;
+        int b = 5;
+
+        double resultado = (double) a / b;
+
+        Console.WriteLine(resultado);
+
+
+        // Conversão explícita com perda de dado
+        double c;
+        int d;
+
+        c = 5.1;
+        d = (int)c;
+
+        Console.WriteLine(d);
+
+        // Conversao implícita
+        int e;
+        double f;
+
+        e = 5;
+        f = e;
+
+        Console.WriteLine(e);
+    }
+}
