50f436ab7baf596ac7d312e710ac53acb3ae3b08
robbfreitas <robertocompromissos@gmail.com>
2022-11-23 16:39:34 -0300

    Entrada de dados.

---
+++ 06EntradaDeDados/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*string entrada = Console.ReadLine();
+        Console.WriteLine(entrada);*/
+
+        string[] v = Console.ReadLine().Split(' ');
+        string a = v[0];
+        string b = v[1];
+        string c = v[2];
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+    }
+}
+
