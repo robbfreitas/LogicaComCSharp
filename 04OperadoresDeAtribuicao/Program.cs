410e078645afc3cd50db3f04bbaa6156eac75679
robbfreitas <robertocompromissos@gmail.com>
2022-11-23 10:11:57 -0300

    Operadores de atribuição

---
+++ 04OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ = | a = 10 | a recebe 10
++= | a += 2 | a recebe a + 2
+-+ | a -= 2 | a recebe a - 2
+*= | a *= 2 | a recebe a * 2
+/= | a /= 2 | a recebe a / 2
+%= | a %= 3 | a recebe a % 3
+ */
+
+class OperadoresDeAtribuicao
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        Console.WriteLine(a);
+
+        a += 2;
+        Console.WriteLine(a);
+
+        a *= 2;
+        Console.WriteLine(a);
+
+        string s = "ABC";
+        Console.WriteLine(s);
+
+        s += "DEF";
+        Console.WriteLine(s);
+
+        //Operadores de aritméticos/atribuição - ++ e -- | a++ ou ++a | a-- ou --a
+
+        int b = 10;
+        b++;
+        Console.WriteLine(b);
+
+        int c = 10;
+        int d = c++;
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+
+        int e = 10;
+        int f = ++e;
+        Console.WriteLine(e);
+        Console.WriteLine(f);
+    }
+
+}
