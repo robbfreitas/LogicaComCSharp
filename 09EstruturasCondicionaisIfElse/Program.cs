d66dbb9641a0e574eadc1b01c30f5a800a217033
robbfreitas <robertocompromissos@gmail.com>
2022-11-28 18:43:02 -0300

    Estruturas Condicionais If e Else + exemplo

---
+++ 09EstruturasCondicionaisIfElse/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Entre com um número inteiro: ");
+        int numeroInteiro = int.Parse(Console.ReadLine());
+
+        if (numeroInteiro % 2 == 0)
+        {
+            Console.WriteLine("Número par");
+        }
+        else
+        {
+            Console.WriteLine("Número ímpar");
+        }
+
+        Console.WriteLine("Que horas são agora? ");
+        double horas = double.Parse(Console.ReadLine());
+
+        if (horas < 12)
+        {
+            Console.WriteLine("Bom dia!");
+        }
+        else if (horas < 18) 
+        {
+            Console.WriteLine("Boa tarde!");
+        }
+        else
+        {
+            Console.WriteLine("Boa noite");
+        }
+    }
+}
