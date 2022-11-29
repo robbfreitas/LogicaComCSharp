b957d4b9916f9a128837e61bcd6be222119551ca
robbfreitas <robertocompromissos@gmail.com>
2022-11-29 16:57:31 -0300

    While

---
+++ 11EstruturaRepetitivaWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿//Encerrar programa com número negativo
+
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite um número: ");
+        double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        while (numero >= 0.0)
+        {
+            double raiz = Math.Sqrt(numero);
+            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Digite outro número: ");
+            numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        Console.WriteLine("Número negativo!");
+    }
+}
