c50b62226ff387dff4eaedf4ad61535548e8b17a
robbfreitas <robertocompromissos@gmail.com>
2022-11-29 18:21:35 -0300

    For

---
+++ 12EstruturaRepetitivaFor/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite quantos números inteiros serão digitados: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int somatorio = 0;
+
+        for (int i = 1; i <= numero; i++)
+        {
+            Console.WriteLine("Valor: #{0}: ", i);
+            int valor = int.Parse(Console.ReadLine());
+            somatorio += valor;
+        }
+
+        Console.WriteLine("Soma igual a: {0}", somatorio);
+    }
+}
